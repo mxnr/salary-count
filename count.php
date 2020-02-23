e30a88bfced42e8f361872d3d61fa384c519d7f4
Dmitry Semenov <mxnr@cloud.upwork.com>
2020-02-22 18:14:27 -0700

    Parsing improvement

--- count.php
+++ count.php
@@ -4,10 +4,14 @@ $rate = readline("\033[1;34m💵 Rate >>> \033[0m");
 // Expected format 1h, 23h23m, 2h06m, 2(number of hours)
 $hours = readline("\033[1;34m⏰ Hours >>> \033[0m");
 
+
+$search  = ['ч', 'h', 'м', 'm'];
+$replace = [':', ':', '', ''];
+
+$hours = str_replace($search, $replace, $hours);
+
 if (strpos($hours, ':') > 0) { 
     $hours = explode(':', $hours);
-} else {
-    $hours = explode('h', $hours);
 }
 
 if (count($hours) > 1 && !empty($hours[1])) {
@@ -17,5 +21,7 @@ if (count($hours) > 1 && !empty($hours[1])) {
 } else {
     $sum = round($hours[0] * $rate, 2);
 }
+
+$sum = str_replace('.', ',',  $sum);
 
 echo "\033[1;32m💰 Salary >>> \033[0;32m\${$sum}\033". PHP_EOL . PHP_EOL;
